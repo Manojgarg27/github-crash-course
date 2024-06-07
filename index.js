74c0657bfea8d821677467cc6cf4b37fbd9e70cc
Manojgarg27 <manojgarg424@gmail.com>
2024-06-07 13:51:34 +0530

    newww

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const dob ='21/12/1995';
 function sayName() {
     console.log("my name is "+mname);
 }
+<<<<<<< HEAD
 
 function newFn() {
     console.log("my name is manoj");
 }
+=======
+>>>>>>> parent of ddf24ae (added isAbove18)
--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function sayHello() {
     console.log("hello");
 }
+
+function myDream() {
+    console.log("i am rich now");
+}
+
+function mySalary(a) {
+    return a>=300000;
+}
