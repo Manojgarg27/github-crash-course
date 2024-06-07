75a9315de3ec050f71e30b8159940941975da793
Manojgarg27 <manojgarg424@gmail.com>
2024-06-07 12:18:33 +0530

    revert

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 const age=28;
-const mname1= 'manoj garg';
+const mname= 'manoj garg';
 const dob ='21/12/1995';
 
 
 function sayName() {
-    console.log("my name is "+mname1);
-}
-
-function isAbove18() {
-    return age>=21
+    console.log("my name is "+mname);
 }
 
 function newFn() {
